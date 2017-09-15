b8658cad57bdc5d896ebbe8694df323dcd8b5015
Anshuman Pati <starapex7@gmail.com>
2017-09-16 00:13:02 +0530

    Rectified errors and updated the database to include new parameter.

--- app/models.py
+++ app/models.py
@@ -9,6 +9,11 @@ class User(db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime)
+        
+    followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+    )
     followed = db.relationship('User', 
                                secondary=followers, 
                                primaryjoin=(followers.c.follower_id == id), 
@@ -49,11 +54,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post %r>' % (self.body)
-    
-followers = db.Table('followers',
-    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
-    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
-)
+
     
     
 
