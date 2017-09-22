035faf215e25b185bddb8a1e82f80a576e7889f0
Anshuman Pati <starapex7@gmail.com>
2017-09-22 22:51:30 +0530

    Bug due to clashes in usernames removed.

--- app/models.py
+++ app/models.py
@@ -44,6 +44,31 @@ class User(db.Model):
     
     def avatar(self, size):
         return ('http://www.gravatar.com/avatar/%s?d=mm&s=%d' % (md5(self.email.encode('utf-8')).hexdigest(), size))
+    
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+            return self
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+            return self
+
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+    
+    @staticmethod
+    def make_unique_nickname(nickname):
+        if User.query.filter_by(nickname=nickname).first() is None:
+            return nickname
+        version = 2
+        while True:
+            new_nickname = nickname + str(version)
+            if User.query.filter_by(nickname=new_nickname).first() is None:
+                break
+            version += 1
+        return new_nickname
 
 
 class Post(db.Model):
